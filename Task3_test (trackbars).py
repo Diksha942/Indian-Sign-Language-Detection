26da246a60a8b15abb947bdcfb8bf86d4df5a00b
Diksha942 <59208103+Diksha942@users.noreply.github.com>
2020-08-10 12:34:18 +0530

    Need to differentiate signs
    0 and 9 ho gaya, 1 and 6 i tried, hua nahi. Voh ek baar ho gaya toh baaki ka kaise krna h voh bhi smjh jayega. Workk on that

--- Task3_test (trackbars).py
+++ Task3_test (trackbars).py
@@ -6,6 +6,9 @@ import math
 def nothing(x):
     pass
 
+def distance(x1,y1, x2,y2):
+    dist = math.sqrt((x1-x2)**2 + (y1-y2)**2)
+    return dist
 
 # creating track bar to find threshold for mask:
 cv.namedWindow('Trackbars')
@@ -24,10 +27,14 @@ if not cap.isOpened():
     exit()
 
 # for selecting skin value in roi:
-hist_flag = 0
+flag = 0
+
+dist = []
+apex = []
 
 # defining kernel:
 kernel = np.ones((3, 3), dtype=np.uint8)
+font = cv.FONT_HERSHEY_SIMPLEX
 
 while True:
 
@@ -42,7 +49,8 @@ while True:
     roi = frame[100:300, 100:300]
 
     cv.rectangle(frame, (100, 100), (300, 300), (0, 255, 0), 0)
-    hsv = cv.cvtColor(roi, cv.COLOR_BGR2HSV)
+    hsv_frame = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    hsv_roi = cv.cvtColor(roi, cv.COLOR_BGR2HSV)
 
     # get current positions of Track-bars:
     l_h = cv.getTrackbarPos("L - H", "Trackbars")
@@ -55,115 +63,146 @@ while True:
     lower_col = np.array([l_h, l_s, l_v])
     upper_col = np.array([u_h, u_s, u_v])
 
-    mask = cv.inRange(hsv, lower_col, upper_col)
-    mask = cv.morphologyEx(mask, cv.MORPH_CLOSE, kernel)
+    mask_frame = cv.inRange(hsv_frame, lower_col, upper_col)
+    mask_frame = cv.morphologyEx(mask_frame, cv.MORPH_CLOSE, kernel)
+    masked_img = cv.bitwise_and(frame, frame, mask=mask_frame)
+    cv.imshow('Trackbars', masked_img)
 
-    # finding contours:
-    contours, hierarchy = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    mask_roi = cv.inRange(hsv_roi, lower_col, upper_col)
+    mask_roi = cv.morphologyEx(mask_roi, cv.MORPH_CLOSE, kernel)
+    mask_roi = cv.GaussianBlur(mask_roi,(5,5), 100)
+    masked_roi = cv.bitwise_and(roi, roi, mask=mask_roi)
 
-    # finding contour of max area(hand):
-    cnt = max(contours, key=lambda y: cv.contourArea(y))
-    #cnt = max(contours, key=cv.contourArea)
+    if cv.waitKey(10)==13 or flag==1:
 
-    # approx the contour a little:
-    epsilon = 0.0005 * cv.arcLength(cnt, True)
-    approx = cv.approxPolyDP(cnt, epsilon, True)
+        flag = 1
+        # finding contours:
+        contours, _ = cv.findContours(mask_roi, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
-    # make convex hull around hand
-    hull = cv.convexHull(cnt)
+        if len(contours)>0:
+            if len(max(contours, key= cv.contourArea)>100):
 
-    # define area of hull and area of hand
-    areahull = cv.contourArea(hull)
-    areacnt = cv.contourArea(cnt)
+                # finding contour of max area(hand):
+                cnt = max(contours, key=lambda y: cv.contourArea(y))
+                rect = cv.minAreaRect(cnt)
+                [(rx,ry),(w,h),_] = rect
+                box = cv.boxPoints(rect)
+                box = np.int0(box)
+                cv.drawContours(roi,[box],0,(0,0,255),1)
+                cv.circle(roi, (int(rx),int(ry)), 2, [255, 0, 0], -1)
+            
+                # approx the contour a little:
+                epsilon = 0.0005 * cv.arcLength(cnt, True)
+                approx = cv.approxPolyDP(cnt, epsilon, True)
 
-    # find the percentage of area not covered by hand in convex hull
-    arearatio = ((areahull - areacnt) / areacnt) * 100
+                # make convex hull around hand
+                hull = cv.convexHull(cnt)
 
-    # find the defects in convex hull with respect to hand
-    hull = cv.convexHull(approx, returnPoints=False)
-    defects = cv.convexityDefects(approx, hull)
+                # define area of hull and area of hand
+                areahull = cv.contourArea(hull)
+                areacnt = cv.contourArea(cnt)
 
-    # l = no. of defects:
-    l = 0
+                # find the percentage of area not covered by hand in convex hull
+                arearatio = ((areahull - areacnt) / areacnt) * 100
 
-    # code for finding no. of defects due to fingers
-    for i in range(defects.shape[0]):
-        s, e, f, d = defects[i, 0]
-        start = tuple(approx[s][0])
-        end = tuple(approx[e][0])
-        far = tuple(approx[f][0])
-        pt = (100, 180)
+                # find the defects in convex hull with respect to hand
+                hull = cv.convexHull(approx, returnPoints=False)
+                defects = cv.convexityDefects(approx, hull)
 
-        # finding length of all sides of triangle:
-        a = math.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
-        b = math.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
-        c = math.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
+                # l = no. of defects:
+                l = 0
 
-        # applying HERON's formula to find area of triangle:
-        s = (a + b + c) / 2
-        ar = math.sqrt(s * (s - a) * (s - b) * (s - c))
+                try:
+                    # code for finding no. of defects due to fingers
+                    for i in range(defects.shape[0]):
+                        s, e, f, d = defects[i, 0]
+                        start = tuple(approx[s][0])
+                        end = tuple(approx[e][0])
+                        far = tuple(approx[f][0])
+                        pt = (100, 180)
 
-        # distance between point and convex hull
-        d = (2 * ar) / a
+                        # finding length of all sides of triangle:
+                        a = math.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
+                        b = math.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
+                        c = math.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
 
-        # applying cosine rule here and finding angle(A):
-        angle = np.rad2deg(math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c)))
+                        # applying HERON's formula to find area of triangle:
+                        s = (a + b + c) / 2
+                        ar = math.sqrt(s * (s - a) * (s - b) * (s - c))
 
-        # ignore angles > 90 and ignore points very close to convex hull(they generally come due to noise)
-        if angle <= 90 and d > 30:
-            l += 1
-            cv.circle(roi, far, 3, [255, 0, 0], -1)
+                        # distance between point and convex hull
+                        d = (2 * ar) / a
 
-        # draw lines around hand
-        cv.line(roi, start, end, [0, 255, 0], 2)
+                        # applying cosine rule here and finding angle(A):
+                        angle = np.rad2deg(math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c)))
 
-    l += 1
+                        # ignore angles > 90 and ignore points very close to convex hull(they generally come due to noise)
+                        if angle <= 90 and d > 20:
+                            l += 1
+                            cv.circle(roi, far, 3, [255, 0, 0], -1)
+                            
+                        # draw lines around hand
+                        cv.line(roi, start, end, [0, 255, 0], 2)
+                except AttributeError:
+                    pass
 
-    # print corresponding gestures which are in their ranges
-    font = cv.FONT_HERSHEY_SIMPLEX
-
-    if l == 1:
-        if areacnt < 2000:
-            cv.putText(frame, 'Put hand in the box', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
-        else:
-            if arearatio < 14.5:
-                cv.putText(frame, '0', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
-            elif 14.5 <= arearatio < 16.5:
-                cv.putText(frame, '6', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
-            elif 16.5 <= arearatio <= 19.5:
-                cv.putText(frame, '1', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
+                l += 1
             else:
-                cv.putText(frame, '9', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
+                pass
 
-    elif l == 2:
-        if arearatio < 13.5:
-            cv.putText(frame, '7', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
+            if l == 1:
+                if arearatio < 14.5:
+                    if 4000<areahull<7200:
+                        cv.putText(frame, '0', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
+                    else:
+                        cv.putText(frame, '9', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
+                else:
+                    p = distance(apex[0][0],rx,apex[0][1],ry)
+                    q = 100
+                    r = distance(apex[0][0],rx+100,apex[0][1],ry)
+
+                    theta = np.rad2deg(math.acos((p ** 2 + q ** 2 - r ** 2) / (2 * p * q)))
+
+                    if theta>90:
+                        cv.putText(frame, '6', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)  #I tried, yeh wala, 1 ad 6 wala, acchese kaam nahi kr rha, please see if u can do this or not
+                    else:
+                        cv.putText(frame, '1', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
+                    
+                '''elif 14.5 <= arearatio < 16.5:
+                    cv.putText(frame, '6', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
+                elif 16.5 <= arearatio <= 19.5:
+                    cv.putText(frame, '1', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)'''
+
+            elif l == 2:
+                if arearatio < 13.5:
+                    cv.putText(frame, '7', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
+                else:
+                    cv.putText(frame, '2', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
+
+            elif l == 3:
+
+                if arearatio < 27:
+                    cv.putText(frame, '3', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
+                else:
+                    cv.putText(frame, '8', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
+
+            elif l == 4:
+                cv.putText(frame, '4', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
+
+            elif l == 5:
+                cv.putText(frame, '5', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
+
+            elif l == 6:
+                cv.putText(frame, 'reposition', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
+
+            else:
+                cv.putText(frame, 'reposition', (10, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
         else:
-            cv.putText(frame, '2', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
+            cv.putText(frame, 'Put hand in the box', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
 
-    elif l == 3:
-
-        if arearatio < 27:
-            cv.putText(frame, '3', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
-        else:
-            cv.putText(frame, '8', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
-
-    elif l == 4:
-        cv.putText(frame, '4', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
-
-    elif l == 5:
-        cv.putText(frame, '5', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
-
-    elif l == 6:
-        cv.putText(frame, 'reposition', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
-
-    else:
-        cv.putText(frame, 'reposition', (10, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
-
-    # Displaying the windows:
-    cv.imshow('mask', mask)
-    cv.imshow('frame', frame)
-
+        cv.imshow('Sign Language Detection', frame)
+        cv.imshow('mask', mask_roi)
+        
     k = cv.waitKey(1) & 0xff
     if k == ord('q'):
         break
