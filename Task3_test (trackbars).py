2c46ae4d7a2c8643dd7c83c5f58f627577410256
Diksha942 <59208103+Diksha942@users.noreply.github.com>
2020-08-13 05:06:03 +0530

    Final Code
    See if guys still need any changes

--- Task3_test (trackbars).py
+++ Task3_test (trackbars).py
@@ -6,41 +6,39 @@ import math
 def nothing(x):
     pass
 
-
 def distance(x1, y1, x2, y2):
     dist = math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
     return dist
 
-def slope(x1,y1,top):
-    theta = math.degrees(math.atan2(top[1]-y1, top[0]-x1))
-    return theta
+def getAngle(a, b, c):
+    ang = math.degrees(math.atan2(c[1]-b[1], c[0]-b[0]) - math.atan2(a[1]-b[1], a[0]-b[0]))
+    return ang + 360 if ang < 0 else ang
 
 
 # creating track bar to find threshold for mask:
 cv.namedWindow('Trackbars')
 cv.resizeWindow('Trackbars', 256, 256)
-cv.createTrackbar("L - H", "Trackbars", 0, 255, nothing)
-cv.createTrackbar("L - S", "Trackbars", 10, 255, nothing)
-cv.createTrackbar("L - V", "Trackbars", 70, 255, nothing)
-cv.createTrackbar("U - H", "Trackbars", 180, 255, nothing)
+cv.createTrackbar("L - H", "Trackbars", 0, 179, nothing)
+cv.createTrackbar("L - S", "Trackbars", 31, 255, nothing)
+cv.createTrackbar("L - V", "Trackbars", 131, 255, nothing)
+cv.createTrackbar("U - H", "Trackbars", 67, 179, nothing)
 cv.createTrackbar("U - S", "Trackbars", 255, 255, nothing)
 cv.createTrackbar("U - V", "Trackbars", 255, 255, nothing)
 
-# STARTING THE VIDEO CAPTURE#
+#we'll require these ahead
+flag = 0
+dist = []
+defect_coord= []
+
+kernel = np.ones((3, 3), dtype=np.uint8)
+font = cv.FONT_HERSHEY_COMPLEX
+
+# STARTING THE VIDEO CAPTURE #
 cap = cv.VideoCapture(0)
 if not cap.isOpened():
     print('Cam\'s not working ')
     exit()
 
-# for selecting skin value in roi:
-flag = 0
-
-dist = []
-apex = []
-
-# defining kernel:
-kernel = np.ones((3, 3), dtype=np.uint8)
-font = cv.FONT_HERSHEY_SIMPLEX
 while True:
 
     ret, frame = cap.read()
@@ -50,73 +48,76 @@ while True:
 
     frame = cv.flip(frame, 1)
 
-    # defining region of interest:
-    roi = frame[100:300, 100:300]
+    #getting the area we wanna work on
+    roi = frame[100:440, 100:400]
 
-    cv.rectangle(frame, (100, 100), (300, 300), (0, 255, 0), 0)
+    #converting them into hsv images for us to work
     hsv_frame = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
     hsv_roi = cv.cvtColor(roi, cv.COLOR_BGR2HSV)
 
-    # get current positions of Track-bars:
-    l_h = cv.getTrackbarPos("L - H", "Trackbars")
-    l_s = cv.getTrackbarPos("L - S", "Trackbars")
-    l_v = cv.getTrackbarPos("L - V", "Trackbars")
-    u_h = cv.getTrackbarPos("U - H", "Trackbars")
-    u_s = cv.getTrackbarPos("U - S", "Trackbars")
-    u_v = cv.getTrackbarPos("U - V", "Trackbars")
+    #flag changes to one when we press enter, and then we want this wiindow to close
+    if flag == 0:
 
-    lower_col = np.array([l_h, l_s, l_v])
-    upper_col = np.array([u_h, u_s, u_v])
+        #get current positions of Track-bars:
+        l_h = cv.getTrackbarPos("L - H", "Trackbars")
+        l_s = cv.getTrackbarPos("L - S", "Trackbars")
+        l_v = cv.getTrackbarPos("L - V", "Trackbars")
+        u_h = cv.getTrackbarPos("U - H", "Trackbars")
+        u_s = cv.getTrackbarPos("U - S", "Trackbars")
+        u_v = cv.getTrackbarPos("U - V", "Trackbars")
 
-    mask_frame = cv.inRange(hsv_frame, lower_col, upper_col)
-    mask_frame = cv.morphologyEx(mask_frame, cv.MORPH_CLOSE, kernel)
-    masked_img = cv.bitwise_and(frame, frame, mask=mask_frame)
-    cv.imshow('Trackbars', masked_img)
+        lower_col = np.array([l_h, l_s, l_v])
+        upper_col = np.array([u_h, u_s, u_v])
 
+        #adjusting the HSV values acc to lighting conditions and skin colour
+        mask_frame = cv.inRange(hsv_frame, lower_col, upper_col)
+        mask_frame = cv.morphologyEx(mask_frame, cv.MORPH_CLOSE, kernel)
+        masked_img = cv.bitwise_and(frame, frame, mask=mask_frame)
+        cv.imshow('Trackbars', masked_img)
+    else:
+        pass
+
+    #creating mask of our hand to get contours
     mask_roi = cv.inRange(hsv_roi, lower_col, upper_col)
     mask_roi = cv.morphologyEx(mask_roi, cv.MORPH_CLOSE, kernel)
     mask_roi = cv.GaussianBlur(mask_roi, (5, 5), 100)
     masked_roi = cv.bitwise_and(roi, roi, mask=mask_roi)
 
+    #when pressed enter, it'll show the blurred mask of roi, with roi and the panl where it displays no.
     if cv.waitKey(10) == 13 or flag == 1:
 
         flag = 1
-        # finding contours:
+
+        #finding contours:
         contours, _ = cv.findContours(mask_roi, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
-        if len(contours) > 0:
+        #ensuring that contours are detected
+        if len(contours) > 1:
             if len(max(contours, key=cv.contourArea) > 100):
 
-                # finding contour of max area(hand):
+                # finding contour of max area(hand)
                 cnt = max(contours, key=lambda y: cv.contourArea(y))
                 rect = cv.minAreaRect(cnt)
-                [(rx, ry), (w, h), _] = rect
+                [(rx, ry), (_,_), _] = rect   #we'll need these co-ordinates to differentiate between 1 and 6
 
-                box = cv.boxPoints(rect)
-                box = np.int0(box)
-                cv.drawContours(roi, [box], 0, (0, 0, 255), 1)
-                cv.circle(roi, (int(rx), int(ry)), 2, [0, 0, 255], -1)
-
-                # approx the contour a little:
+                # approx the contour a bit
                 epsilon = 0.0005 * cv.arcLength(cnt, True)
                 approx = cv.approxPolyDP(cnt, epsilon, True)
 
                 # make convex hull around hand
                 hull = cv.convexHull(cnt)
 
-                # define area of hull and area of hand
+                # we'll need the area and area ratio to tell the numbers apart
                 areahull = cv.contourArea(hull)
                 areacnt = cv.contourArea(cnt)
 
-                # find the percentage of area not covered by hand in convex hull
                 arearatio = ((areahull - areacnt) / areacnt) * 100
 
                 # find the defects in convex hull with respect to hand
                 hull = cv.convexHull(approx, returnPoints=False)
                 defects = cv.convexityDefects(approx, hull)
-
-                # l = no. of defects:
-                l = 0
+                
+                l = 0 # the no. of defects
 
                 try:
                     # code for finding no. of defects due to fingers
@@ -142,83 +143,88 @@ while True:
                         # applying cosine rule here and finding angle(A):
                         angle = np.rad2deg(math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c)))
 
-                        # ignore angles > 90 and ignore points very close to convex hull(they generally come due to noise)
-                        if angle <= 90 and d > 20:
+                        # ignore angles > 80 and ignore points very close to convex hull(they generally come due to noise)
+                        if angle <= 80 and d > 20:
                             l += 1
-                            cv.circle(roi, far, 3, [255, 0, 0], -1)
+                            cv.circle(roi, far, 3, [255,153,51], -1)
+                            defect_coord.append(far)
 
                         # draw lines around hand
-                        cv.line(roi, start, end, [0, 255, 0], 2)
+                        cv.line(roi, start, end, [51,153,255], 2)
+
                 except AttributeError:
                     pass
-
-                l += 1
             else:
                 pass
 
-            theta = slope(rx, ry, far)
+            #define the panel where no. will be displayed
+            cv.rectangle(frame,(100,400), (400,440),(96,96,96),-1)
 
-            if l == 1:
+            #setting conditions for numbers to display, according to hand gestures
+            if l == 0:
                 if arearatio < 14.5:
-                    if 4000 < areahull < 7200:
-                        cv.putText(frame, '0', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
-                    else:
-                        cv.putText(frame, '9', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
-                else:
-                    '''p = distance(apex[0][0], rx, apex[0][1], ry)
-                    q = 100
-                    r = distance(apex[0][0], rx + 100, apex[0][1], ry)'''
 
+                    #the gesture 9 has a largere area than 0
+                    if 10000< areahull < 18000: 
+                        cv.putText(frame, '0', (105,435), font, 1, (0, 255, 128), 3, cv.LINE_AA)
+                    else:
+                        cv.putText(frame, '9', (105,435), font, 1, (0, 255, 128), 3, cv.LINE_AA)
+                else:
+
+                    #getting the co-ordinates of topmost point i.e out fingertip
                     topmost = tuple(cnt[cnt[:,:,1].argmin()][0])
-                    theta = slope(rx,ry,topmost)
-                    if  -125 < theta < -85:
-                        cv.putText(frame, '1', (0, 50), font, 2, (0, 0, 255), 3,cv.LINE_AA)
-                    # I tried, yeh wala, 1 ad 6 wala, acchese kaam nahi kr rha, please see if u can do this or not
+                    theta = getAngle((rx+100,ry),(rx,ry),topmost) #remeber the minAreaRect? The angle between finger tip, and the horizontal of center of
+                                                                #rect co-ordinate will help us distinguish 1 and 6
+                    if  260 < theta:
+                        cv.putText(frame, '1', (105,435), font, 1, (0, 255, 128), 3,cv.LINE_AA)
                     else:
-                        print(theta)
-                        cv.putText(frame, '6', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
+                        cv.putText(frame, '6', (105,435), font, 1, (0, 255, 128), 3, cv.LINE_AA)
 
-                '''elif 14.5 <= arearatio < 16.5:
-                    cv.putText(frame, '6', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
-                elif 16.5 <= arearatio <= 19.5:
-                    cv.putText(frame, '1', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)'''
+            elif l == 1:
+                
+                theta = getAngle((400,400),(250,400),defect_coord[-1]) #getting the angle of defects from the horizontal
 
-            elif l == 2:
-
-                if -125 < theta < 110:
-                    cv.putText(frame, '2', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
+                if theta>245:
+                    cv.putText(frame, '2', (105,435), font, 1, (0, 255, 128), 3, cv.LINE_AA)
                 else:
-                    cv.putText(frame, '7', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
+                    cv.putText(frame, '7', (105,435), font, 1, (0, 255, 128), 3, cv.LINE_AA)
+
+                if len(defect_coord)>5:     #we are kinda appending every co-ordinate of defect from each frame to
+                    del defect_coord[0:3]   #the array. Deleting the previous unwanted elements would save memory
+                    
+            elif l == 2:
+                
+                theta1 = getAngle((400,400),(250,400),defect_coord[-1])
+                theta2 = getAngle((400,400),(250,400),defect_coord[-2])
+            
+                if theta1<=242 and theta2>=240:
+                    cv.putText(frame, '8', (105,435), font,1, (0, 255, 128), 3, cv.LINE_AA)
+                else:
+                    cv.putText(frame, '3', (105,435), font, 1, (0, 255, 128), 3, cv.LINE_AA)
+
+                if len(defect_coord)>5:
+                    del defect_coord[0:3]
 
             elif l == 3:
-
-                if arearatio < 27 :
-                    cv.putText(frame, '3', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
-                else:
-                    cv.putText(frame, '8', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
+                cv.putText(frame, '4', (105,435), font, 1, (0, 255, 128), 3, cv.LINE_AA)
 
             elif l == 4:
-                cv.putText(frame, '4', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
-
-            elif l == 5:
-                cv.putText(frame, '5', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
-
-            elif l == 6:
-                cv.putText(frame, 'reposition', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
-
+                cv.putText(frame, '5', (105,435), font, 1, (0, 255, 128), 3, cv.LINE_AA)
+                
             else:
-                cv.putText(frame, 'reposition', (10, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
+                cv.putText(frame, 'Reposition', (105,435), font, 1, (0, 255, 128), 3, cv.LINE_AA)
         else:
-            cv.putText(frame, 'Put hand in the box', (0, 50), font, 2, (0, 0, 255), 3, cv.LINE_AA)
+            cv.rectangle(frame,(100,400), (400,440),(96,96,96),-1)
+            cv.putText(frame, 'Put hand in the box', (105,435), font, 0.7, (0, 255, 128), 3, cv.LINE_AA) #if no contour is detected
 
-        cv.imshow('Sign Language Detection', frame)
+        cv.destroyWindow('Trackbars') #closing the trackbars window as it's no longer in use
+        cv.imshow('Sign Language Detection', roi)
         cv.imshow('mask', mask_roi)
-
+    
     k = cv.waitKey(1) & 0xff
     if k == ord('q'):
         break
 
-
-
 cap.release()
 cv.destroyAllWindows()
+
